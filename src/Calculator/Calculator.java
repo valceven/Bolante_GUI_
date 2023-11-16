98ff24b8b924eb8a87bb5f0964d17c004cc255e2
LAB12_USER ON WS40 <L12Y16W40@citulab.net>
2023-11-16 16:54:00 +0800

    2nd Commit

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,48 @@
+package Calculator;
+
+import javax.swing.*;
+
+public class Calculator extends JFrame{
+
+
+    private JPanel jpanel;
+    private JTextField tfNumber1;
+    private JTextField tfNumber2;
+    private JComboBox cbOperations;
+    private JButton btnCompute;
+    private JLabel lblResult;
+    private JTextField result;
+
+    public Calculator(){
+
+        btnCompute.addActionListener(e -> {
+            int num1 = Integer.parseInt(tfNumber1.getText());
+            int num2 = Integer.parseInt(tfNumber2.getText());
+            int res = 0;
+
+            if(cbOperations.getSelectedIndex() == 0){
+                res = num1 + num2;
+            }else if(cbOperations.getSelectedIndex() == 1){
+                res = num1 - num2;
+            }else if(cbOperations.getSelectedIndex() == 2){
+                res = num1 * num2;
+            }else if(cbOperations.getSelectedIndex() == 3){
+                res = num1 / num2;
+            }
+            result.setText(String.valueOf(res));
+
+        });
+    }
+
+
+    public static void main(String[] args) {
+
+        Calculator app = new Calculator();
+        app.setContentPane(app.jpanel);
+        app.setSize(500,500);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.setTitle("Leap Year Checker");
+
+    }
+}
